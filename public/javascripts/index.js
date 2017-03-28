88db21503c18a30ec34630d3543ac8c0e95ef1a9
jesseluo <luo_sj@163.com>
2017-03-28 23:27:56 +0800

    s

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -24,12 +24,33 @@
 	var noLogin = document.querySelector('.no-login');
 	var loginRegPop = document.querySelector('.login-reg-pop');
 
-	noLogin.addEventListener('click', function(evt){
-		loginRegPop.style.display = 'flex';
-	})
+	if (noLogin) {
+		noLogin.addEventListener('click', function(evt){
+			loginRegPop.style.display = 'flex';
+		})	
+	}
 
 	var popClose = document.querySelector('.pop-close');
-	popClose.addEventListener('click', function(evt){
-		loginRegPop.style.display = 'none';
-	})
+	if (popClose) {
+		popClose.addEventListener('click', function(evt){
+			loginRegPop.style.display = 'none';
+		})	
+	}
+	
 })()	
+
+;(function addCollection(){
+	document.addEventListener('click', function(evt){
+		var elem = evt.target;
+		if (elem.classList.contains('collection')) {
+			var a = elem.parentNode.querySelector('a');
+			var aId = a.href;
+			elem.classList.toggle('collection-active');
+			if (elem.classList.contains('collection-active')){
+				//数据库删除收藏
+			}else{
+				//数据库添加收藏
+			}
+		}
+	})
+})()
